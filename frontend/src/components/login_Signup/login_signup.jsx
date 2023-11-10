dd55e0ef2710ee975bd0bc10137201f970d54e88
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-11-10 19:01:13 +0200

    refactor logo name

--- frontend/src/components/login_Signup/login_signup.jsx
+++ frontend/src/components/login_Signup/login_signup.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import './login_signup.css'
 import user from '../assets/user.png'
-import email from '../assets/email.png'
-import password from '../assets/lock.png'
+import emailLogo from '../assets/email.png'
+import passwordLogo from '../assets/lock.png'
 
 const LoginSignup = () => {
 
@@ -13,13 +13,13 @@ const LoginSignup = () => {
 
     const submit = async () => {
         try {
-          const response = await axios.post('http://localhost:3001/api/receiveData', {email, password});
-          console.log(response.email);
-          console.log(response.password);
+            const response = await axios.post('http://localhost:3001/api/receiveData', { email, password });
+            console.log(response.email);
+            console.log(response.password);
         } catch (error) {
-          console.error('Error sending data to server:', error);
+            console.error('Error sending data to server:', error);
         }
-      };
+    };
 
 
     return (
@@ -30,31 +30,31 @@ const LoginSignup = () => {
                 <div className="underline"></div>
             </div>
 
-                <div className="inputs">
-                    {action === "Log In" ? <div></div> : <div className="input">
-                        <img src={user} alt="" />
-                        <input type="text" placeholder='Username' />
+            <div className="inputs">
+                {action === "Log In" ? <div></div> : <div className="input">
+                    <img src={user} alt="" />
+                    <input type="text" placeholder='Username' />
                 </div>}
             </div>
 
             <div className="inputs">
                 <div className="input">
-                    <img src={email} alt="" />
-                    <input type="email" onChange={(e) => {setEmail(e.target.value)}} placeholder='Email' />
+                    <img src={emailLogo} alt="" />
+                    <input type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder='Email' />
                 </div>
             </div>
 
             <div className="inputs">
                 <div className="input">
-                    <img src={password} alt="" />
-                    <input type="password" onChange={(e) => {setPassword(e.target.value)}} placeholder='Password' />
+                    <img src={passwordLogo} alt="" />
+                    <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder='Password' />
                 </div>
             </div>
 
 
             <div>
-                {action === "Sign Up" ? <div></div> : <input type = "submit" onClick={submit}/>
-}
+                {action === "Sign Up" ? <div></div> : <input type="submit" onClick={submit} />
+                }
             </div>
 
             <div className="submit-container">
