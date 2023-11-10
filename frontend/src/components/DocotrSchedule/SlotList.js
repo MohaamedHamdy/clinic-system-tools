0adb37050d602df4afaf484c05459361dec8c612
mariamali10 <119268837+mariamali10@users.noreply.github.com>
2023-11-11 01:28:24 +0200

    added slot list

---
+++ frontend/src/components/DocotrSchedule/SlotList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SlotList = ({ slots }) => {
+  return (
+    <div>
+      <h2>Slots</h2>
+      <ul>
+        {slots.map((slot, index) => (
+          <li key={index}>
+            Date: {slot.date}, Start Time: {slot.startTime}, End Time: {slot.endTime}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SlotList;
