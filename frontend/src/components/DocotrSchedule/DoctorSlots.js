20f0e80df526f02d3f40c2a945fc8aef140198f0
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-11-11 17:18:16 +0200

    view slots in doctor screen done

--- frontend/src/components/DocotrSchedule/DoctorSlots.js
+++ frontend/src/components/DocotrSchedule/DoctorSlots.js
@@ -3,19 +3,19 @@ import SlotForm from './SlotForm';
 import SlotList from './SlotList';
 
 const DoctorSlots = () => {
-    const [slots, setSlots] = useState([]);
+  const [slots, setSlots] = useState([]);
 
-    const addSlot = (newSlot) => {
-      setSlots([...slots, newSlot]);
-    };
-  
-    return (
-      <div className="App">
-        <h1>Doctor schedule</h1>
-        <SlotForm addSlot={addSlot} />
-        <SlotList slots={slots} />
-      </div>
-    );
+  const addSlot = (newSlot) => {
+    setSlots([...slots, newSlot]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Doctor schedule</h1>
+      <SlotForm addSlot={addSlot} />
+      <SlotList />
+    </div>
+  );
 };
 
 export default DoctorSlots;
--- frontend/src/components/DocotrSchedule/SlotList.js
+++ frontend/src/components/DocotrSchedule/SlotList.js
@@ -4,28 +4,30 @@ import axios from 'axios';
 const SlotList = () => {
 
   const [slots, setSlots] = useState([]);
+  const [doctorSlotId, setDoctorId] = useState(4);
 
-  useEffect(() => {
-    axios.get('http://localhost:3001/api/viewSlots')
-      .then(response => {
-        console.log(response.data); // Log the data to the console
-        setSlots(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching slots:', error);
-      });
-  }, []);
-
+  const fetchData = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/api/viewSlots', { doctorSlotId });
+      console.log('Response:', response); // Log the entire response object
+      console.log('Data fetched successfully.', response.data);
+      setSlots(response.data);
+    } catch (error) {
+      console.error('Error fetching slots:', error);
+    }
+  };
 
   return (
 
 
     <div>
       <h2>Available Slots</h2>
+      <button onClick={fetchData}>Show Slots</button>
+
       <ul>
         {slots.map(slot => (
           <li key={slot.id}>
-            Date: {slot.date}, Start Time: {slot.start_time}, Slot Id: {slot.slot_id}
+            Date: {slot.date}, Start Time: {slot.start_time},
           </li>
         ))}
       </ul>
