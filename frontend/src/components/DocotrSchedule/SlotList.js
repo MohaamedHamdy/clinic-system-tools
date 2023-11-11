1bf686281d066b8982124526477ab90bfc01686b
MohaamedHamdy <m7mdhamdy45@gmail.com>
2023-11-11 16:18:20 +0200

    get slots from database into doctor screen

--- frontend/src/components/DocotrSchedule/SlotList.js
+++ frontend/src/components/DocotrSchedule/SlotList.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const SlotList = ({ slots }) => {
+
+  const [slotts, setSlots] = useState([]);
+
+  axios.get('http://localhost:3001/api/viewSlots')
+    .then(response => {
+      setSlots(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching slots:', error);
+    });
+
   return (
+
+
     <div>
-      <h2>Slots</h2>
+      <h2>Available Slots</h2>
       <ul>
-        {slots.map((slot, index) => (
-          <li key={index}>
-            Date: {slot.date}, Start Time: {slot.start_time}, End Time: {slot.endTime}
+        {slots.map(slot => (
+          <li key={slot.id}>
+            Date: {slot.date}, Start Time: {slot.start_time}, End Time: {slot.end_time}
           </li>
         ))}
       </ul>
+      <p> there is ==== {slotts.length}</p>
     </div>
   );
 };
